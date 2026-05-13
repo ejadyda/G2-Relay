5f585315a59fe5dc8b85c136e4f74acb5adb63c0
ejadyda <ejadyda@gmail.com>
2026-05-13 06:49:46 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,11 +8,17 @@ export TARGET_PORT="${TARGET_PORT:-48560}"
 export TARGET_SCHEME="${TARGET_SCHEME:-http}"
 
 export VLESS_PATH="${VLESS_PATH:-/}"
-export LINK_NAME="${LINK_NAME:-g2ray-lite}"
+export LINK_NAME="${LINK_NAME:-g2ray-lwq4w11y}"
 
 echo
 echo "Starting g2ray-lite-forwarder-go..."
 echo "Target: ${TARGET_SCHEME}://${TARGET_HOST}:${TARGET_PORT}"
 echo
+
+if command -v gh >/dev/null 2>&1 && [ -n "${CODESPACE_NAME:-}" ]; then
+  echo "Trying to make port 3000 public..."
+  gh codespace ports visibility 3000:public -c "$CODESPACE_NAME" || true
+  echo
+fi
 
 go run ./main.go
